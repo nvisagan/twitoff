31b03ab573b73021cf868dc77449c0b0d48cc9e1
Navo Visagan <navo17@gmail.com>
2019-10-16 22:08:00 -0700

    Day 3

--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -8,7 +8,7 @@ from .models import DB, Tweet, User
 TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
                                    config('TWITTER_CONSUMER_SECRET'))
 TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
-                              config('TWITTER_ACCESS_TOKEN_SECRET'))
+                              config('TWITTER_ACcESS_TOKEN_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
 
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
